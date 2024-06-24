1133990c9d1e91dcb2be1dc5af37113d20e5ce9a
Ekaterina Varnak <116022974+Hsbnn@users.noreply.github.com>
2024-06-24 14:47:01 +0300

    Add files via upload

---
+++ frontend/src/data/sampleImages.js
@@ -0,0 +1,8 @@
+const sampleImages = [
+    { url: 'https://via.placeholder.com/150' },
+    { url: 'https://via.placeholder.com/150' },
+    { url: 'https://via.placeholder.com/150' },
+    { url: 'https://via.placeholder.com/150' },
+  ];
+  
+  export default sampleImages;
